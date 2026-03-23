8d5d0c7e43bdbd53a0980ffaab23339de6325f1a
Lorensha <102773745+lorensha@users.noreply.github.com>
2026-03-23 23:26:11 +0200

    Update ET_model.py

--- Decoy_Paper_Project/ET_model.py
+++ Decoy_Paper_Project/ET_model.py
@@ -8,11 +8,11 @@ from Calculations import *
 import os
 
 # ── CONFIG ──────────────────────────────────────────────────────────────────
-DATA_DIR = '/mnt/cargo/lorensha/PXD010154_tonsil_trypsin/decoy_variants/'
-RESULTS_DIR = '/mnt/cargo/lorensha/results2/'
+DATA_DIR = '/mnt/cargo/lorensha/PXD010154_tonsil_trypsin/reversed_decoys/'
+RESULTS_DIR = '/mnt/cargo/lorensha/reversed_decoys_results/'
 
 files = {
-    'SAMEA7718763_percolator_input_extended.tsv': 'NT_SAMEA7718763_decoy_variants.csv'
+    'SAMEA7718756_percolator_input_extended.tsv': 'NT_SAMEA7718756_reversed_decoys.csv'
 }
 
     # Setup base estimator: Feature variable & class labels
