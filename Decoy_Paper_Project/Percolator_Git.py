b21e8c8ad0d5581cdcbd909069bf69a97b8fa2bc
Lorensha <102773745+lorensha@users.noreply.github.com>
2026-06-23 15:13:30 +0200

    Add files via upload

---
+++ Decoy_Paper_Project/Percolator_Git.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+import glob
+
+percolator_files_dir = 'mnt/cargo/lorensha/PXD010154_tonsil_trypsin/reversed_decoys/percolator/'
+cols = ["PSMId", "score", "q-value", "posterior_error_prob", "peptide", "proteinIds"]
+
+subfolders = [f.path for f in os.scandir(percolator_files_dir) if f.is_dir()]
+
+for folder in subfolders:
+    target_file = os.path.join(folder, "percolator.target.peptides.txt")
+    decoy_file  = os.path.join(folder, "percolator.decoy.peptides.txt")
+
+    target_psms = pd.read_table(target_file ,sep='\t',
+                           names=cols, header=0, usecols=range(len(cols)))
+
+    decoy_psms = pd.read_table(decoy_file ,sep='\t',
+                           names=cols, header=0, usecols=range(len(cols)))
+    decoy_psms["Label_perc"] = -1
+    target_psms["Label_perc"] = 1
+
+    Percolator_results = pd.concat([target_psms, decoy_psms], ignore_index=True)
+
+    folder_name = os.path.basename(folder)
+    output_path = os.path.join(folder, f"{folder_name}_combined.txt")
+    Percolator_results.to_csv(output_path, sep="\t", index=False)
+
+    print(f"Combined: {output_path}  ({Percolator_results.shape[0]} rows)")
